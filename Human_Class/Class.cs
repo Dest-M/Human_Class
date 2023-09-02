ec8817ed2fe5ee759a97cd14cd1563bca40a83e1
hp <hp@LAPTOP-QLT1H0K9>
2023-09-02 19:48:57 +0300

    Add project files.

---
+++ Human_Class/Class.cs
@@ -0,0 +1,77 @@
+﻿namespace Human_Class
+{
+    internal class Human
+    {
+        string name;
+        string gender;
+        int age = 0;
+        public Human parent1 = null;
+        public Human parent2 = null;
+        public Human partner = null;
+        public Human child = null;
+        public Human(string name, string gender, int age)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+        }
+        public Human(string name, string gender, int age, Human p1, Human p2)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+
+            if (p1 != null && p2 != null)
+            {
+                if (p1.age > age && p2.age > age)
+                {
+                    parent1 = p1;
+                    parent2 = p2;
+                    p1.child = this;
+                    p2.child = this;
+                    p1.partner = p2;
+                    p2.partner = p1;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR Age of child can not be higher than parents");
+                }
+            }
+            else
+            {
+                Console.WriteLine(name + "ERROR Comp");
+            }
+
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public string getGender()
+        {
+            return gender;
+        }
+        public string getAge()
+        {
+            return Convert.ToString(age);
+        }
+        public override string ToString()
+        {
+            string str = "";
+            if (parent1 != null)
+            {
+                str += "Parent 1: " + parent1.name + " ";
+            }
+            if (parent2 != null)
+            {
+                str += "Parent 2:" + parent2.name + " ";
+            }
+            if (child != null)
+            {
+                str += "Child: " + child.name + " ";
+            }
+            return name + " " + gender + " " + Convert.ToString(age) + " " + str;
+        }
+    }
+}
+
---
+++ Human_Class/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 5;
+            Human[] h = new Human[count];
+
+            h[0] = new Human("Sasha", "Male", 45);
+            h[1] = new Human("Alexis", "Female", 39);
+            h[2] = new Human("Roy", "Male", 23, h[0], h[1]);
+            h[3] = new Human("Amy", "Female", 24);
+            h[4] = new Human("Lily", "Female", 2, h[2], h[3]);
+
+            Console.WriteLine(h[0].GetName() + " Partner " + h[0].partner.GetName());
+            Console.WriteLine(h[1].GetName() + " Child " + h[0].child.GetName());
+            Console.WriteLine(h[3].ToString());
+            Console.WriteLine(h[4].ToString());
+
+        }
+    }
+}
